56371dbd0205a882d54ef51db79a4c404335dedf
Naturalsoul <raul.d.carrasco@outlook.com>
2016-06-23 00:05:42 -0400

    Menu added to Calculator class.

--- src/BasicCalculatorJava/Calculator.java
+++ src/BasicCalculatorJava/Calculator.java
@@ -1,11 +1,37 @@
 package BasicCalculatorJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /* This is a simple calculator. It's my first repository in Github with Java
  * code. I hope I'll continue coding and learning :)
  *
  */
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        int selection;
+
+        while(true) {
+            Scanner reader = new Scanner(System.in);
+            menu();
+
+            try {
+                System.out.print("Choose an option: ");
+                selection = reader.nextInt();
+                System.exit(0);
+            } catch (InputMismatchException ex) {
+                System.out.println("\nOnly numbers, pls!\n");
+            }
+        }
+    }
+
+    private static void menu() {
+        System.out.println("--------------------");
+        System.out.println("Hey!! what u want to do? :D");
+        System.out.println("1) I want a sum!");
+        System.out.println("\n");
+        System.out.println("--------------------");
+
+
     }
 }
