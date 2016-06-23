d7d840b9c2e15e9f5f5a9d2879107513db371736
Naturalsoul <maxr7@live.cl>
2016-06-22 20:14:35 -0400

    Testing!

---
+++ src/BasicCalculatorJava/Calculator.java
@@ -0,0 +1,11 @@
+package BasicCalculatorJava;
+
+/* This is a simple calculator. It's my first repository in Github with Java
+ * code. I hope I'll continue coding and learning :)
+ *
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
