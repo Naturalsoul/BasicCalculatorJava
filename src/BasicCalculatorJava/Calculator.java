a944bd4d654d9165380e4fa0c83ddf3e0d36dbf8
Naturalsoul <raul.d.carrasco@outlook.com>
2016-06-24 17:41:54 -0400

    Added the sum and an exit option.

--- src/BasicCalculatorJava/Calculator.java
+++ src/BasicCalculatorJava/Calculator.java
@@ -1,5 +1,9 @@
 package BasicCalculatorJava;
 
+import BasicCalculatorJava.Operations.sum;
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,7 +13,7 @@ import java.util.Scanner;
  */
 public class Calculator {
     public static void main(String[] args) {
-        int selection;
+        int selection = 0;
 
         while(true) {
             Scanner reader = new Scanner(System.in);
@@ -18,9 +22,28 @@ public class Calculator {
             try {
                 System.out.print("Choose an option: ");
                 selection = reader.nextInt();
-                System.exit(0);
             } catch (InputMismatchException ex) {
                 System.out.println("\nOnly numbers, pls!\n");
+            }
+
+            switch (selection) {
+                case 1:
+                    int checkNum;
+                    ArrayList<Integer> nums = new ArrayList<>();
+                    System.out.println("Type all the numbers u want to sum separated with spaces:");
+                    reader.nextLine();
+
+                    for (String token; (token = reader.findInLine("[1-9]")) != null;)
+                        nums.add(Integer.valueOf(token));
+
+                    sum Sum = new sum(nums);
+                    System.out.println("\nTotal: " + Sum.doSum());
+
+                    break;
+
+                case 9:
+                    System.out.println("See ya later!! ;D");
+                    System.exit(0);
             }
         }
     }
@@ -29,6 +52,7 @@ public class Calculator {
         System.out.println("--------------------");
         System.out.println("Hey!! what u want to do? :D");
         System.out.println("1) I want a sum!");
+        System.out.println("9) Good bye!");
         System.out.println("\n");
         System.out.println("--------------------");
 
